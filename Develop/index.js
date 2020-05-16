d7e483c3929ff4942681da2b79b3d872ab2a5be7
Andy Sanchez <andysanchez726@gmail.com>
2020-05-16 12:35:56 -0500

    accidentally started working on the user input in develop

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,28 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the README?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of the README?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the usage instructions?'
+    },
+    {
+        
+    }
 ];
 
 // function to write README file
